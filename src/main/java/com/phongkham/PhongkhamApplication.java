ab6c59da9906319edc944d62fcd8d1f9db9ef704
Đỗ Trung Đức <duc010298@gmail.com>
2018-08-12 09:32:15 +0700

    first commit
    Setup mySQL

---
+++ src/main/java/com/phongkham/PhongkhamApplication.java
@@ -0,0 +1,12 @@
+package com.phongkham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhongkhamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhongkhamApplication.class, args);
+    }
+}
