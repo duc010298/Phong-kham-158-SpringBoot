9a3009f7e8fb05a1798c5b736685df6cdc8a8a7d
Đỗ Trung Đức <duc010298@gmail.com>
2018-08-17 18:56:58 +0700

    email send
    work with app of Sony

--- src/main/java/com/phongkham/controller/CustomerHiddenController.java
+++ src/main/java/com/phongkham/controller/CustomerHiddenController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.thymeleaf.TemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -21,14 +20,11 @@ public class CustomerHiddenController {
 
     private CustomerHiddenRepository customerHiddenRepository;
     private JavaMailSender javaMailSender;
-    private TemplateEngine templateEngine;
 
     @Autowired
-    public CustomerHiddenController(CustomerHiddenRepository customerHiddenRepository, JavaMailSender javaMailSender,
-                                    TemplateEngine templateEngine) {
+    public CustomerHiddenController(CustomerHiddenRepository customerHiddenRepository, JavaMailSender javaMailSender) {
         this.customerHiddenRepository = customerHiddenRepository;
         this.javaMailSender = javaMailSender;
-        this.templateEngine = templateEngine;
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -37,17 +33,18 @@ public class CustomerHiddenController {
         //send mail
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        String htmlMsg = "<style>.page{width: 46.5625rem; margin: 3.125rem auto 1.25rem auto; background-color: #fff; " +
-                "overflow: hidden; box-shadow: 0.3125rem 0.3125rem 0.3125rem #7F7979; font-family: \"Times New Roman\", " +
-                "Times, serif;}.page-container{margin: 1.875rem 2.8125rem;}.page-container table, td{border: 1px solid " +
-                "#F5E8E8; border-collapse: collapse;}.page-container td{padding: 0.3125rem;}</style><div class=\"page\"> " +
-                "<div class=\"page-container\">";
+        String htmlMsg = "<!DOCTYPE html><html> <head> <style>body{background-color: #F0F0F0;}.page{width: 46.5625rem;" +
+                " margin: 3.125rem auto 1.25rem auto; background-color: #fff; overflow: hidden; box-shadow: 0.3125rem" +
+                " 0.3125rem 0.3125rem #7F7979; font-family: \"Times New Roman\", Times, serif;}.page-container{margin:" +
+                " 1.875rem 2.8125rem;}.page-container table, td{border: 1px solid #F5E8E8; border-collapse: collapse;}" +
+                ".page-container td{padding: 0.3125rem;}</style> </head> <body><div class=\"page\"> <div class=\"page-" +
+                "container\"> <div>";
         htmlMsg += customerEntity.getReport();
-        htmlMsg += "</div></div>";
+        htmlMsg += "</div></div></div></body></html>";
 
         helper.setFrom("phongkham158@gmail.com");
-        helper.setTo("duc010298@gmail.com");
-        helper.setSubject("Test send HTML email");
+        helper.setTo("ducpanzer@gmail.com");
+        helper.setSubject("Báo cáo");
         helper.setText(htmlMsg, true);
 
         javaMailSender.send(message);
