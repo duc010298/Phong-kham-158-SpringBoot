922dd7a4abc17b90489e8d2da17e886a8a3a9116
DoTrungDuc <duc010298@gmail.com>
2018-09-28 20:54:15 +0700

    config security

--- src/main/java/com/duc010298/phongkham158/config/WebSecurityConfig.java
+++ src/main/java/com/duc010298/phongkham158/config/WebSecurityConfig.java
@@ -38,8 +38,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
 
         http.authorizeRequests().antMatchers("/login").anonymous();
-//        http.authorizeRequests().antMatchers("/", "/form/**", "/customer",
-//                "/customer/**", "/customerHidden", "/customerHidden/**").hasRole("MEMBER");
+        http.authorizeRequests().antMatchers("/", "/customer/**", "/form/**",
+                "/report/**", "/setting/manager-form/**").hasRole("MEMBER");
+        http.authorizeRequests().antMatchers("/setting/manager-clinic/**", "/customerHidden/**").hasRole("MASTER");
+        http.authorizeRequests().antMatchers("/setting/manager-user/**").hasRole("ADMIN");
 
         http.authorizeRequests().and().formLogin()
                 .loginPage("/login")
