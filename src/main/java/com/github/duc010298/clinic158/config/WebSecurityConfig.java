a16a490b26754cc4d8aff540043d4bb2257c102c
Do Trung Duc <duc010298@gmail.com>
2019-01-09 19:14:35 +0700

    disable csrf

--- src/main/java/com/github/duc010298/clinic158/config/WebSecurityConfig.java
+++ src/main/java/com/github/duc010298/clinic158/config/WebSecurityConfig.java
@@ -35,6 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http
                 .authorizeRequests()
                 .antMatchers("/resources/**", "/bootstrap/**", "/jquery/**").permitAll()
