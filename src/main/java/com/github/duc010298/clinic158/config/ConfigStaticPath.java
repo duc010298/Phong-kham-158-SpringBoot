a5f97bd048105a920f22f2b8ebee266eea44144f
Do Trung Duc <duc010298@gmail.com>
2019-02-17 21:06:10 +0700

    cache everything

--- src/main/java/com/github/duc010298/clinic158/config/ConfigStaticPath.java
+++ src/main/java/com/github/duc010298/clinic158/config/ConfigStaticPath.java
@@ -1,9 +1,12 @@
 package com.github.duc010298.clinic158.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebMvc
@@ -12,6 +15,6 @@ public class ConfigStaticPath implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
-                .addResourceLocations("classpath:/static/");
+                .addResourceLocations("classpath:/static/").setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));;
     }
 }
