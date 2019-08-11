17bb33cc82c5dcc2d3903e7ebe0cf6cf09036696
Đỗ Trung Đức <duc010298@gmail.com>
2019-08-11 10:36:45 +0700

    .

--- src/main/resources/db/migration/V2__Initial_Data.sql
+++ src/main/resources/db/migration/V2__Initial_Data.sql
@@ -2,19 +2,13 @@ insert into app_user (user_name, encrypted_password, full_name)
 values ('duc010298', '$2a$10$hjBz774Yg4Fff44DYseK4.w4p27w2enR0W.QxSxlIXA.TcxS2bYV.', 'Đỗ Trung Đức');
 
 insert into app_role (id, role_name)
-values (1, 'ROLE_GLOBAL_ADMIN');
+values (1, 'ROLE_ADMIN');
 
 insert into app_role (id, role_name)
-values (2, 'ROLE_CLINIC_ADMIN');
-
-insert into app_role (id, role_name)
-values (3, 'ROLE_USER');
+values (2, 'ROLE_USER');
 
 insert into user_role
 values (1, 1);
 
 insert into user_role
 values (1, 2);
-
-insert into user_role
-values (1, 3);
