a2ebb236075d84dcad5cb6ec3695daae56cd18ca
Đỗ Trung Đức <duc010298@gmail.com>
2019-08-11 09:57:23 +0700

    Update main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -48,10 +48,31 @@ let removeSignAndLowerCase = (str) => {
     return str;
 }; //remove sign and lower case string
 
+let disableResizeAllTable = () => {
+    let editor = document.getElementById('editor');
+    let listTable = editor.getElementsByTagName('table');
+    for (let table of listTable) {
+        $(table).colResizable({
+            disable: true
+        });
+    }
+}; //disable col resize all table
+
+let enableResizeAllTable = () => {
+    let editor = document.getElementById('editor');
+    let listTable = editor.getElementsByTagName('table');
+    for (let table of listTable) {
+        $(table).colResizable({
+            liveDrag: true,
+            draggingClass: "dragging"
+        });
+    }
+};  //enable col resize all table
+
 document.getElementById('btn-print').onclick = () => {
-    //TODO disable col resize
+    disableResizeAllTable();
     $("#print").printThis({importCSS: false});
     setTimeout(function () {
-        //TODO enable col resize
+        enableResizeAllTable();
     }, 500);
 }; //print report
